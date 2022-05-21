7a16b2bd025da974e23e6414fe6521747e70d52b
RomanJelinek <roman.jelinek91@gmail.com>
2022-05-21 13:51:07 +0200

    footer adjusted

--- src/components/homepage/footer/Footer.js
+++ src/components/homepage/footer/Footer.js
@@ -7,8 +7,7 @@ import styles from './Footer.module.css';
 const Footer = () => {
     return (
       <div className={styles.footerContainer}>
-        <a href="/login">Přihlásit se</a>
-        <p>by Roman Jelínek</p>
+        <p>by RJ</p>
       </div>
     );
 };
